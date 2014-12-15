b00ddf4c0cb07a4f0e67b8b91d56b5a9881f3240
runclimbhike <trailrunnerforlife@gmail.com>
2014-12-15 15:11:24 -0600

    added CreateHeader definition

--- Dashboard.py
+++ Dashboard.py
@@ -32,15 +32,36 @@ def ProgramHeader():
     # Show all brews currently in secondary fermentation.
     Reporting.secondary_state()
     # Show all brews currently in bottle conditioning.
-    #Reporting.bottled_state()
+    Reporting.bottled_state()
     Reporting.ready_to_drink()
     Reporting.expired()
 
     print(pounds*31)
 
+
+def ViewHeader():
+    # Clear the console.
+    os.system('cls')
+    # Print the header
+    print(pounds*31)
+    print(tab*5, '      *** VIEW & SEARCH MENU ***')
+    print(pounds*31)
+    print(tab*3, '1. Beers in Primary\t2. All Beers\t0. Back to Main Control Console')
+
+def CreateHeader():
+    # Clear the console.
+    os.system('cls')
+    # Print the header
+    print(pounds*31)
+    print(tab*6, '*** CREATE & UPDATE MENU ***')
+    print(pounds*31)
+    print(tab*3, '1.Create new beer\t2.Update beer state\t0.Back to Main Control Console')
+
+
+
 # Create a list of the possible options for the Main Control Console so that user input can be verified.
 options = ['1', '2', '3', '4', '0']
-prompt = 'Select a number: '
+prompt = 'Select a number > '
 while True:
     # Call the Program Header to print it out.
     ProgramHeader()
@@ -52,53 +73,48 @@ while True:
     inp = input(prompt)
     # Verify user input
     if inp not in options:
-        print('Select a number: ')
+        print('Select a number > ')
         continue
 ### View & Search ###
     if inp == '1':
         ProgramHeader()
-
-        while True:
-            print(tab*6, '*** VIEW & SEARCH MENU ***')
-            print(tab*3, '1. Beers in Primary\t2. All Beers\t0. Back to Main Control Console')
-            data_requested = input('Select a number: ')
-
+        ViewHeader()
+        data_requested = input('Select a number > ')
+        while data_requested != "0":
+            ProgramHeader()
+            ViewHeader()
             if data_requested == '2':
                 print(new_line)
                 Reporting.show_all()
                 print(new_line)
+                print(tab*3, '\t0. Back to MAIN MENU')
+                data_requested = input('Select a number > ')
+                if data_requested == '0':
+                    break
+                else:
+                    print('Please select a number from the menu.')
 
-            elif data_requested == '0':
-                #print(new_line*66)
-                break
+
 
 
 ### Create & Update ###
     if inp == '2':
         ProgramHeader()
-        while True:
-            tab = '\t'
-            space = ' '
+        CreateHeader()
+        data_requested = input('Select a number > ')
 
-            print(tab*6, '*** CREATE & UPDATE MENU ***')
-            print(space*3, '1.Create new beer\t2.Update beer state\t0.Back to Main Control Console')
-            data_requested = input('Select a number: ')
+        while data_requested != "0":
+
 
             if data_requested == '1':
+                print(new_line)
                 Lifecycle.new_brew()
 
             elif data_requested == '2':
                 print(new_line)
                 Lifecycle.change_state()
-
-            elif data_requested == '3':
-                Lifecycle.bottling()
-
-
-
-
-            elif data_requested == '0':
                 break
+
 
 ### System Settings ###
     if inp == '3':
@@ -107,7 +123,7 @@ while True:
             tab = '\t'
             print(tab*6, '*** SYSTEM SETTINGS MENU***')
             print(tab*3, '1. Placeholder\t2. Placeholder\t0. Back to Main Control Console')
-            data_requested = input('Select a number: ')
+            data_requested = input('Select a number > ')
 
             if data_requested == '0':
                 break
@@ -120,20 +136,21 @@ while True:
         Lifecycle.close()
         ProgramHeader()
         print('Saving changes, closing database connection and exiting the console.')
-        time.sleep(.5)
+        time.sleep(.2)
         ProgramHeader()
         print('Saving changes, closing database connection and exiting the console..')
-        time.sleep(.5)
+        time.sleep(.2)
         ProgramHeader()
         print('Saving changes, closing database connection and exiting the console...')
-        time.sleep(.5)
+        time.sleep(.2)
         ProgramHeader()
         print('Saving changes, closing database connection and exiting the console....')
-        time.sleep(.5)
+        time.sleep(.2)
         ProgramHeader()
         print('Saving changes, closing database connection and exiting the console.....')
-        time.sleep(.5)
+        time.sleep(.2)
         ProgramHeader()
         print('Saving changes, closing database connection and exiting the console.....DONE')
+        time.sleep(1)
         exit()
 
